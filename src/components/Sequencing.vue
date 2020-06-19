7c572fa0d3928005c5c6cd51b0fcf58fae335618
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-06-19 22:15:21 +0900

    入力数に応じてボックスを表示

--- src/components/Sequencing.vue
+++ src/components/Sequencing.vue
@@ -1,12 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <div class="input_box">
+      <div>
+        <p>表示するボックス数</p>
+        <p>
+          <input type="number" v-model.number="numberOfBoxes" min="0" />
+        </p>
+      </div>
+      <div v-for="item in numberOfBoxes" :key="item">
+        <input type="text" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Sequencing extends Vue {}
+export default class Sequencing extends Vue {
+  private numberOfBoxes = 0;
+}
 </script>
 
 <style>
