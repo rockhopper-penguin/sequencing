0c5b7c3e655a6c28e205505d654d3ff6f912d3ba
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-06-19 22:20:18 +0900

    配列に値を挿入

--- src/components/Sequencing.vue
+++ src/components/Sequencing.vue
@@ -8,8 +8,9 @@
         </p>
       </div>
       <div v-for="item in numberOfBoxes" :key="item">
-        <input type="text" />
+        <input type="text" v-model="saveNames[item - 1]" />
       </div>
+      <button @click="debug">値を出力</button>
     </div>
   </div>
 </template>
@@ -20,6 +21,11 @@ import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class Sequencing extends Vue {
   private numberOfBoxes = 0;
+  private saveNames: string[] = [];
+
+  debug() {
+    console.log(this.saveNames);
+  }
 }
 </script>
 
