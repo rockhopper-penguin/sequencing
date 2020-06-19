f13e3a8dfe42ebe2c55e4c8a3df64147c1bd5485
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-06-19 22:40:01 +0900

    シャッフル機能の追加

--- src/components/Sequencing.vue
+++ src/components/Sequencing.vue
@@ -10,7 +10,12 @@
       <div v-for="item in numberOfBoxes" :key="item">
         <input type="text" v-model="saveNames[item - 1]" />
       </div>
-      <button @click="debug">値を出力</button>
+      <div>
+        <p>
+          <button @click="shuffle">シャッフル</button>
+        </p>
+        <p v-for="item in shuffleNames" :key="item">{{ item }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -20,11 +25,24 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class Sequencing extends Vue {
+  /** 表示するボックス数 */
   private numberOfBoxes = 0;
+  /** ボックスに入力される値 */
   private saveNames: string[] = [];
+  /** シャッフル用配列 */
+  private shuffleNames: string[] = [];
 
-  debug() {
-    console.log(this.saveNames);
+  private shuffle() {
+    // 配列の初期化
+    this.shuffleNames = [];
+    // シャッフル用配列にボックスの入力値配列をディープコピー
+    this.shuffleNames = [...this.saveNames];
+    for (let i = this.shuffleNames.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const tmp = this.shuffleNames[i];
+      this.shuffleNames[i] = this.shuffleNames[j];
+      this.shuffleNames[j] = tmp;
+    }
   }
 }
 </script>
